60bb203572b45db5c594d27b2517e894c257020a
gnaghiyeva <gulnar.nagiyeva17@gmail.com>
2025-03-13 12:17:35 +0400

    field with multipart form

--- src/main/java/org/example/movieapi/controller/AuthController.java
+++ src/main/java/org/example/movieapi/controller/AuthController.java
@@ -4,11 +4,9 @@ import org.example.movieapi.dtos.user.LoginDto;
 import org.example.movieapi.dtos.user.RegisterDto;
 import org.example.movieapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -16,13 +14,13 @@ public class AuthController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
+    @PostMapping(value = "/register", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> register(@ModelAttribute RegisterDto registerDto) {
         return ResponseEntity.ok(userService.registerUser(registerDto));
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+    @PostMapping(value = "/login", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> login(@ModelAttribute LoginDto loginDto) {
         return ResponseEntity.ok(userService.loginUser(loginDto));
     }
 }
